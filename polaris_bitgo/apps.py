35cdf3d2a2755acc8202f7cef8647acb63ccd2fe
Caio Teixeira <caio.xd63@hotmail.com>
2021-08-31 11:54:07 -0300

    rename project

---
+++ polaris_bitgo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PolarisBitGoConfig(AppConfig):
+    name = "polaris_bitgo"
+    verbose_name = "Django Polaris BitGo"
--- polaris_custodial/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PolarisCustodialConfig(AppConfig):
-    name = "polaris_custodial"
-    verbose_name = "Django Polaris Custodial"
